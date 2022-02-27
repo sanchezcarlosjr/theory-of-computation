a01fb8de0cccb1539f33075e87c27e332ddcccb7
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-02-26 22:23:24 -0800

    fix(grammar):  transform should spread in correct order

--- src/grammars/grammarUpsert.tsx
+++ src/grammars/grammarUpsert.tsx
@@ -3,19 +3,19 @@ import {Create, Edit} from 'react-admin';
 import {GrammarForm} from "./grammarForm";
 import {Grammar} from "./grammar";
 
-export function transformGrammarUpsert(record: {id: string, grammar: string}):  Grammar {
+export function transformGrammarUpsert(record: {id: string, grammar: string}|Grammar):  Grammar {
     const nameMatcher = record.grammar.match(/(?<name>[\w\d,\\_-]+)=(.*)/i);
     const terminalSymbolMatcher = record.grammar.match(/terminal:\\left\\lbrace(?<terminal_symbols>[\w \d,\\_-]+)\\right\\rbrace/i);
     const nonterminalSymbolMatcher = record.grammar.match(/nonterminal:\\left\\lbrace(?<nonterminal_symbols>[\w \d,\\_-]+)\\right\\rbrace/i);
     const productionRuleMatcher = record.grammar.match(/production\\_rules:\\left\\lbrace(?<production_rules>[\w \d,\\_-]+)((\\rbrace\\right)|(\\right\\rbrace)){2,}/);
     const startSymbolMatcher = record.grammar.match(/start\\_symbol:(?<start_symbol>[\w\d\\_-]+)/);
     return {
+        ...record,
         name: nameMatcher?.groups?.name ?? "",
         terminal_symbols: terminalSymbolMatcher?.groups?.terminal_symbols.trim() ?? "",
         nonterminal_symbols: nonterminalSymbolMatcher?.groups?.nonterminal_symbols.trim() ?? "",
         production_rules: productionRuleMatcher?.groups?.production_rules.trim() ?? "",
         start_symbol: startSymbolMatcher?.groups?.start_symbol.trim() ?? "",
-        ...record
     };
 }
 
