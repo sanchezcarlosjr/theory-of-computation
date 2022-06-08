9c078d435e497af416d1c4d875c40f31488bd001
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-06-08 00:05:13 -0700

    feat(parser): dead code since it is unuseful transform grammar

--- src/grammars/grammarUpsert.tsx
+++ src/grammars/grammarUpsert.tsx
@@ -1,32 +1,16 @@
 import * as React from "react";
 import {Create, Edit} from 'react-admin';
 import {GrammarForm} from "./grammarForm";
-import {GrammarRecord} from "./grammarRecord";
 
-export function transformGrammarUpsert(record: {id: string, grammar: string}|GrammarRecord):  GrammarRecord {
-    const nameMatcher = record.grammar.match(/(?<name>[\w\d,\\_-]+)=(.*)/i);
-    const terminalSymbolMatcher = record.grammar.match(/terminal:\\left\\lbrace(?<terminal_symbols>[\w \d,*+.\-=!@#$%^&()~`><?\\{}[\]_-]+)\\right\\rbrace/i);
-    const nonterminalSymbolMatcher = record.grammar.match(/nonterminal:\\left\\lbrace(?<nonterminal_symbols>[\w \d,\\_-]+)\\right\\rbrace/i);
-    const productionRuleMatcher = record.grammar.match(/production\\_rules:\\left\\lbrace(?<production_rules>[\w |\d,*+.\-=!@#$%^&()~`><?\\{}[\]_-]+)((\\rbrace\\right)|(\\right\\rbrace)){2,}/);
-    const startSymbolMatcher = record.grammar.match(/start\\_symbol:(?<start_symbol>[\w\d\\_-]+)/);
-    return {
-        ...record,
-        name: nameMatcher?.groups?.name ?? "",
-        terminal_symbols: terminalSymbolMatcher?.groups?.terminal_symbols.trim() ?? "",
-        nonterminal_symbols: nonterminalSymbolMatcher?.groups?.nonterminal_symbols.trim() ?? "",
-        production_rules: productionRuleMatcher?.groups?.production_rules.trim() ?? "",
-        start_symbol: startSymbolMatcher?.groups?.start_symbol.trim() ?? "",
-    };
-}
 
 export const GrammarCreate = (props: any) => (
-    <Create transform={transformGrammarUpsert} {...props}>
+    <Create  {...props}>
         <GrammarForm redirect="edit" />
     </Create>
 );
 
 export const GrammarEdit = (props: any) => (
-    <Edit transform={transformGrammarUpsert} {...props}>
+    <Edit {...props}>
         <GrammarForm  redirect={false} />
     </Edit>
 );
